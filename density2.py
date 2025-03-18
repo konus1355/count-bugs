68bdb81e807597344b78f209576393d499eb6b87
konus1355 <ktchesla@vt.edu>
2025-03-18 14:16:24 -0400

    Update density2.py

--- density2.py
+++ density2.py
@@ -35,8 +35,8 @@ mu = 1 / (np.pi * (D50 ** 2) * np.log(1 + (Rmax ** 2 / D50 ** 2))) * (1 / spTfer
 
 # Compute densities correctly
 Ms = chi2_df['M'].values
-lower_bounds = [(mu / 2) * chi2_df.loc[chi2_df['M'] == M, 'Chi2_lower'].values[0] * 1000 for M in Ms]
-upper_bounds = [(mu / 2) * chi2_df.loc[chi2_df['M'] == M, 'Chi2_upper'].values[0] * 1000 for M in Ms]
+lower_bounds = [(mu / 2) * chi2_df.loc[chi2_df['M'] == M, 'Chi2_lower'].values[0] * 1000 if M > 0 else 0 for M in Ms]
+upper_bounds = [(mu / 2) * chi2_df.loc[chi2_df['M'] == M, 'Chi2_upper'].values[0] * 1000 if M > 0 else (mu / 2) * 7.37776 * 1000 for M in Ms]
 most_probable = [mu * M * 1000 for M in Ms]
 
 # Debugging Table for verification
